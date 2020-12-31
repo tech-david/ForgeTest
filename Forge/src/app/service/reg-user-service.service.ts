2be1e9e7c472485f3c920f609cb37c22ae4a28f0
David Mejia <43398304+tech-david@users.noreply.github.com>
2020-12-31 11:21:27 -0700

    Update reg-user-service.service.ts
    commented out user import

--- Forge/src/app/service/reg-user-service.service.ts
+++ Forge/src/app/service/reg-user-service.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {User} from '../User';
+//import {User} from '../User';
 
 @Injectable({
   providedIn: 'root'
