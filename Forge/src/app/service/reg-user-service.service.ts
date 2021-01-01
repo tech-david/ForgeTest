e7ad83f2b83c233e2884b67b15075955cf730e09
unknown <david1990m@gmail.com>
2021-01-01 15:46:18 -0700

    updated user import

--- Forge/src/app/service/reg-user-service.service.ts
+++ Forge/src/app/service/reg-user-service.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {User} from '/src/app/user';
+import {User} from '../user';
 
 @Injectable({
   providedIn: 'root'
